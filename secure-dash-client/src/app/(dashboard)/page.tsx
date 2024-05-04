7d3418cc7b36ae1efc79e1f15fc60cce2e3f13bd
Brijesh Kumar <bk10895@gmail.com>
2024-05-04 14:17:42 +0530

    feat: add secure dashboard page, header and menu components
    - Added new secure dashboard page for authenticated users
    - Implemented header component with navigation and title
    - Created a menu component with navigational links and current page highlighting
    - Updated layout to include the newly created header component
    - Modified tailwind.config.ts to include color customization

---
+++ secure-dash-client/src/app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DashboardPage = () => {
+  return (
+    <>
+      <div>Secure Dahsboard</div>
+      <p>Only authenticated users can see this page</p>
+    </>
+  );
+};
+
+export default DashboardPage;
