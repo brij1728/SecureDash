48d4155004cc450edc440db02fd1ecaa74b6e8f1
Brijesh Kumar <bk10895@gmail.com>
2024-05-06 21:52:21 +0530

    feat: implement user registration functionality
    - Add new authentication controller with a register function
    - Update utils to support salt generation, password hashing, and session token generation for user registration
    - Export new utils from the utility index

--- secure-dash-server/src/utils/authTokenUtils.ts
+++ secure-dash-server/src/utils/authTokenUtils.ts
@@ -4,15 +4,20 @@ import crypto from 'crypto';
 
 const secret = "BRIJESH-REST-API";
 
-export const random = (req: Request, res: Response) => {
-  const length = parseInt(req.query.length as string) || 32; 
+export const generateSalt = (): string => {
+  const length = 128; 
   const buffer = crypto.randomBytes(length);
-  const token = buffer.toString('base64');
-  res.send(token);
+  return buffer.toString('base64');
 };
 
-export const authentication = (salt: string, password: string) => {
+
+export const hashPassword = (salt: string, password: string) => {
   const hash = crypto.createHmac('sha512', [salt, password].join('/'));
   hash.update(secret);
   return hash.digest('hex');
+};
+
+export const generateSessionToken = (): string => {
+    const buffer = crypto.randomBytes(64); 
+    return buffer.toString('hex');
 };
--- secure-dash-server/src/utils/index.ts
+++ secure-dash-server/src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './validateEnv';
+export * from './authTokenUtils';
