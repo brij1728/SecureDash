c5bd038955bc1454f5d75f44d7abdb3c96351d74
Brijesh Kumar <bk10895@gmail.com>
2024-05-07 13:08:20 +0530

    refactor: update import paths and error handling in authController
    - Change import paths from absolute to relative for better compatibility
    - Improve error handling in the user registration function to include console error logging
    - Comment out unused '/api' route in app.ts

--- secure-dash-server/src/controllers/authController.ts
+++ secure-dash-server/src/controllers/authController.ts
@@ -1,32 +1,32 @@
 import { Request, Response } from "express";
-import { createUser, getUserByEmail } from "db";
-import { generateSalt, generateSessionToken, hashPassword } from "utils";
+import { createUser, getUserByEmail } from '../db';
+import { generateSalt, generateSessionToken, hashPassword } from '../utils';
 
 export const register = async (req: Request, res: Response) => {
   try {
-	const { username, email, password } = req.body;
-	if (!username || !email || !password) {
-	  return res.status(400).json({message: "Missing required fields"});
-	}
-	const existingUser = await getUserByEmail(email);
-	if (existingUser) {
-	  return res.sendStatus(400).json({message: "User already exists"});
-	}
-	const salt = generateSalt()
-	const hashedPassword = hashPassword(password, salt);
-	const sessionToken = generateSessionToken();
-	const user = await createUser({
-	  username,
-	  email,
-	  authentication: {
-		password: hashedPassword,
-		salt,
-		sessionToken,
-	  },
-	});
-	res.status(201).json({sessionToken: user.authentication.sessionToken});
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Missing required fields" });  
+    }
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });  
+    }
+    const salt = generateSalt();
+    const hashedPassword = hashPassword(password, salt);
+    const sessionToken = generateSessionToken();
+    const user = await createUser({
+      username,
+      email,
+      authentication: {
+        password: hashedPassword,
+        salt,
+        sessionToken,
+      },
+    });
+    return res.status(201).json({ user }).end();  
   } catch (error) {
-	res.status(500).json({message: "Internal server error"});
+    console.error("Registration error:", error);
+    return res.status(500).json({ message: "Internal server error" });  
   }
-  
 }
